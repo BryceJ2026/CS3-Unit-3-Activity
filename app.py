df808e9b1023bddeac9e58a90b08e5ef9c9d2480
Bryce? <bryce.jones2026@gbwl.org>
2026-03-03 20:05:10 +0000

    Set up html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, requests
+import json
 
  # Create instance of Flask
  app = Flask(__name__)
@@ -6,7 +7,10 @@ from flask import Flask, render_template, requests
 #Homepage route
 @app.route("/")
 def index():
-   return render_template("index.html")
+   #Open a JSON data file in ead mode
+   with open('static/data.json', 'r') as file:
+      data_dictionary = json.load(file)
+   return render_template("index.html", data=data_dictionary)
 
 # RUN THE APP (or type flask run in terminal)
 if __name__ == "__main__": 
